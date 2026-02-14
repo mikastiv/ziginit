a79719a9e5070ff1f7342b62cceacd06e0fb97d0
mikastiv <mikastiv@outlook.com>
2026-02-13 22:44:13 -0400

    update flake

--- flake.nix
+++ flake.nix
@@ -8,17 +8,60 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, zig, zls, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      zig,
+      zls,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
+        lib = nixpkgs.lib;
+        fs = lib.fileset;
         pkgs = import nixpkgs { inherit system; };
-      in {
+        version = "0.1.0";
+        zigPkg = zig.packages.${system}."0.15.2";
+      in
+      {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = [
-            zig.packages.${system}."0.15.2"
+            zigPkg
             zls.packages.${system}.zls
           ];
         };
-      });
-}
 
+        packages.default = pkgs.stdenvNoCC.mkDerivation {
+          pname = "ziginit";
+          version = version;
+          src = fs.toSource {
+            root = ./.;
+            fileset = fs.intersection (fs.fromSource (lib.sources.cleanSource ./.)) (
+              fs.unions [
+                ./src
+                ./build.zig
+                ./build.zig.zon
+              ]
+            );
+          };
+
+          strictDeps = true;
+          nativeBuildInputs = [ zigPkg ];
+
+          zigBuildFlags = [
+            "-Doptimize=ReleaseSafe"
+          ];
+
+          configurePhase = ''
+            export ZIG_GLOBAL_CACHE_DIR=$TEMP/.cache
+          '';
+
+          buildPhase = ''
+            zig build install --color off --prefix $out
+          '';
+        };
+      }
+    );
+}
