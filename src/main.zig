a71bdb5c62a273b9103dd2f3b4011c74328c2f8e
mikastiv <mikastiv@outlook.com>
2026-02-14 01:13:21 -0400

    add options

--- src/main.zig
+++ src/main.zig
@@ -8,28 +8,86 @@ var stdout_buffer: [1024]u8 = undefined;
 var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
 const stdout = &stdout_writer.interface;
 
+const Fingerprint = packed struct(u64) {
+    id: u32,
+    checksum: u32,
+
+    fn int(f: Fingerprint) u64 {
+        return @bitCast(f);
+    }
+};
+
+const usage =
+    \\usage: ziginit [options] <project name>
+    \\
+    \\options:
+    \\  --help
+    \\  --flake-package
+    \\  --zig-version=[version]
+    \\
+;
+
+fn fatal(err: anyerror) noreturn {
+    std.process.fatal("{t}\n", .{err});
+}
+
+pub fn cutPrefix(comptime T: type, slice: []const T, prefix: []const T) ?[]const T {
+    return if (std.mem.startsWith(T, slice, prefix)) slice[prefix.len..] else null;
+}
+
 pub fn main() !void {
     var arena: std.heap.ArenaAllocator = .init(std.heap.page_allocator);
     defer arena.deinit();
 
     const allocator = arena.allocator();
 
-    const args = try std.process.argsAlloc(allocator);
-    if (args.len != 2) {
-        try stderr.print("usage: {s} <project name>\n", .{if (args.len > 0) args[0] else "ziginit"});
-        return error.MissingArgument;
+    const args = (try std.process.argsAlloc(allocator))[1..];
+
+    var pname: ?[]const u8 = null;
+    var zig_version: std.SemanticVersion = .{ .major = 0, .minor = 15, .patch = 2 };
+    var is_flake_package = false;
+
+    for (args) |arg| {
+        if (std.mem.startsWith(u8, arg, "-")) {
+            if (std.mem.eql(u8, arg, "--help")) {
+                try stderr.writeAll(usage);
+                try stderr.flush();
+                return;
+            } else if (std.mem.eql(u8, arg, "--flake-package")) {
+                is_flake_package = true;
+            } else if (cutPrefix(u8, arg, "--zig-version=")) |version| {
+                zig_version = try .parse(version);
+            } else {
+                fatal(error.InvalidOption);
+            }
+        } else {
+            if (pname != null) {
+                fatal(error.DuplicateArgument);
+            }
+            pname = arg;
+        }
     }
 
-    const project_name = args[1];
+    const project_name = pname orelse fatal(error.MissingArgument);
+
     try std.fs.cwd().makeDir(project_name);
+
+    const fingerprint: Fingerprint = .{
+        .id = std.crypto.random.intRangeLessThan(u32, 1, 0xffffffff),
+        .checksum = std.hash.Crc32.hash(project_name),
+    };
 
     const project_dir = try std.fs.cwd().openDir(project_name, .{});
     try project_dir.makeDir("src");
 
     try writeFile(project_dir, "build.zig", build_zig, .{project_name});
-    try writeFile(project_dir, "build.zig.zon", build_zig_zon, .{project_name});
+    try writeFile(project_dir, "build.zig.zon", build_zig_zon, .{ project_name, fingerprint.int(), zig_version });
     try writeFile(project_dir, "src/main.zig", main_zig, .{});
-    try writeFile(project_dir, "flake.nix", flake, .{});
+    if (is_flake_package) {
+        try writeFile(project_dir, "flake.nix", flake_package, .{zig_version});
+    } else {
+        try writeFile(project_dir, "flake.nix", flake, .{zig_version});
+    }
     try writeFile(project_dir, ".envrc", envrc, .{});
     try writeFile(project_dir, ".gitignore", gitignore, .{});
 }
@@ -63,6 +121,17 @@ const build_zig =
     \\    }});
     \\
     \\    b.installArtifact(exe);
+    \\
+    \\    const run_step = b.step("run", "Run the app");
+    \\
+    \\    const run_cmd = b.addRunArtifact(exe);
+    \\    run_step.dependOn(&run_cmd.step);
+    \\
+    \\    run_cmd.step.dependOn(b.getInstallStep());
+    \\
+    \\    if (b.args) |args| {{
+    \\        run_cmd.addArgs(args);
+    \\    }}
     \\}}
     \\
 ;
@@ -70,9 +139,9 @@ const build_zig =
 const build_zig_zon =
     \\.{{
     \\    .name = .{s},
-    \\    .version = "0.0.0",
-    \\    .fingerprint = 0x8c736521f9f54213,
-    \\    .minimum_zig_version = "0.15.2",
+    \\    .version = "0.1.0",
+    \\    .fingerprint = 0x{x},
+    \\    .minimum_zig_version = "{f}",
     \\    .dependencies = .{{}},
     \\    .paths = .{{
     \\        "build.zig",
@@ -111,13 +180,83 @@ const flake =
     \\      in {{
     \\        devShells.default = pkgs.mkShell {{
     \\          nativeBuildInputs = [
-    \\            zig.packages.${{system}}."0.15.2"
+    \\            zig.packages.${{system}}."{f}"
     \\            zls.packages.${{system}}.zls
     \\          ];
     \\        }};
     \\      }});
     \\}}
     \\
+;
+
+const flake_package =
+    \\{{
+    \\  description = "zig flake";
+    \\
+    \\  inputs = {{
+    \\    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    \\    zig.url = "github:mitchellh/zig-overlay";
+    \\    zls.url = "github:zigtools/zls";
+    \\    flake-utils.url = "github:numtide/flake-utils";
+    \\  }};
+    \\
+    \\  outputs =
+    \\    {{
+    \\      self,
+    \\      nixpkgs,
+    \\      zig,
+    \\      zls,
+    \\      flake-utils,
+    \\    }}:
+    \\    flake-utils.lib.eachDefaultSystem (
+    \\      system:
+    \\      let
+    \\        lib = nixpkgs.lib;
+    \\        fs = lib.fileset;
+    \\        pkgs = import nixpkgs {{ inherit system; }};
+    \\        version = "0.1.0";
+    \\        zigPkg = zig.packages.${{system}}."{f}";
+    \\      in
+    \\      {{
+    \\        devShells.default = pkgs.mkShell {{
+    \\          nativeBuildInputs = [
+    \\            zigPkg
+    \\            zls.packages.${{system}}.zls
+    \\          ];
+    \\        }};
+    \\
+    \\        packages.default = pkgs.stdenvNoCC.mkDerivation {{
+    \\          pname = "ziginit";
+    \\          version = version;
+    \\          src = fs.toSource {{
+    \\            root = ./.;
+    \\            fileset = fs.intersection (fs.fromSource (lib.sources.cleanSource ./.)) (
+    \\              fs.unions [
+    \\                ./src
+    \\                ./build.zig
+    \\                ./build.zig.zon
+    \\              ]
+    \\            );
+    \\          }};
+    \\
+    \\          strictDeps = true;
+    \\          nativeBuildInputs = [ zigPkg ];
+    \\
+    \\          zigBuildFlags = [
+    \\            "-Doptimize=ReleaseSafe"
+    \\          ];
+    \\
+    \\          configurePhase = ''
+    \\            export ZIG_GLOBAL_CACHE_DIR=$TEMP/.cache
+    \\          '';
+    \\
+    \\          buildPhase = ''
+    \\            zig build install --color off --prefix $out
+    \\          '';
+    \\        }};
+    \\      }}
+    \\    );
+    \\}}
 ;
 
 const envrc =
